5dfc99bc8ea98ae5af564a65b6f85de84a750833
Krish2673 <krishpawar073@gmail.com>
2025-09-06 19:10:38 +0530

    responsiveness

--- real-estate-visualizer/src/components/Navbar.jsx
+++ real-estate-visualizer/src/components/Navbar.jsx
@@ -1,16 +1,44 @@
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">🏠 Real Estate Visualizer</div>
 
-      <ul className="navbar-links fancy-menu">
-        <li><NavLink to="/" end>Home</NavLink></li>
-        <li><NavLink to="/visualizations">Visualizations</NavLink></li>
-        <li><NavLink to="/explorer">Explorer</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
+      <div
+        className={`hamburger ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
+        <li>
+          <NavLink to="/" end onClick={() => setMenuOpen(false)}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/visualizations" onClick={() => setMenuOpen(false)}>
+            Visualizations
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/explorer" onClick={() => setMenuOpen(false)}>
+            Explorer
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
